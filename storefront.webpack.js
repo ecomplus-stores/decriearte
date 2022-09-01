68ad36bbf169027afa486b43fd55455a88c7e3a4
eduvlemes <edu.vlemes@gmail.com>
2022-09-01 20:16:53 -0300

    update

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -3,10 +3,6 @@ const path = require('path')
 module.exports = () => ({
   resolve: {
     alias: {
-      //'./html/APrices.html': path.resolve(__dirname, 'template/js/blow-gummies/html/APrices.html'),
-      //'./html/AccountPoints.html': path.resolve(__dirname, 'template/js/blow-gummies/html/AccountPoints.html'),
-      //'./html/DiscountApplier.html': path.resolve(__dirname, 'template/js/blow-gummies/html/DiscountApplier.html'),
-      //'./js/DiscountApplier.js': path.resolve(__dirname, 'template/js/blow-gummies/js/DiscountApplier.js'),
       './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'),
       './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.js'),
     }
--- template/js/blow-gummies/pages.js
+++
@@ -1,6 +0,0 @@
-// Add your custom JavaScript for storefront pages here.
-if (storefront && storefront.context && storefront.context.resource === 'products') {
-    ecomCart.on('addItem', () => { 
-        window.location = '/app/#/cart/' 
-    })
-}
